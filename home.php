8768500648d28794d48f11ca1baa82c5034d3432
chrysan95 <aeslehcyn@gmail.com>
2025-02-15 19:57:27 +0700

    asik selesai

--- home.php
+++ home.php
@@ -15,7 +15,7 @@ if(!isset($_SESSION['user_id'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Dashboard</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -102,9 +102,9 @@ if(!isset($_SESSION['user_id'])){
             margin-top: 10px;
         }
         .card_ctn .btn-primary{
-            background-color: #EAFAEA;
+            background-color: #6E8E59;
             border: none;
-            color:#6E8E59;
+            color:#dff3d3;
             padding:  5px 20px 5px 20px; 
             font-size: 16px; 
             font-family: 'Poppins';
@@ -126,7 +126,7 @@ if(!isset($_SESSION['user_id'])){
             justify-content: space-between;
         }
         .card-title{
-            color: #6E8E59;
+            color: #831d38c5;
             font-weight: bold;
         }
         .card-text{
